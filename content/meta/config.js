3a05eb7270c55a0f071df0798ce40a1159c1962a
Rameez Khan <rameezkhan.sa@gmail.com>
2018-12-21 09:53:36 +0200

    Tweak footnotes and meta data

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "Rameez Khan", // <title>
+  shortSiteTitle: "Rameez Khan", // <title> ending for posts and pages
+  siteDescription: "Rameez Khan | Personal Site",
+  siteUrl: "https://rameezkhan.me",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Rameez Khan",
+  authorTwitterAccount: "RameezKhanSA",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "Rameez Khan",
+  headerSubTitle: "DevOps Engineer",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "Rameez Khan - Personal Site",
+  manifestShortName: "Rameez Khan", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -32,8 +32,8 @@ module.exports = {
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/rameezk" },
+    { name: "twitter", url: "https://twitter.com/RameezKhanSA" },
+    { name: "linkedin", url: "http://linkedin.com/in/khanrameez" }
   ]
 };
