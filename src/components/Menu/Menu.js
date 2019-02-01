57b146a5b28f5b3a0c5e8d100fc6f98888b3a493
Rameez Khan <rameezkhan.sa@gmail.com>
2019-02-01 08:36:33 +0200

    Remove some pages

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -26,8 +26,6 @@ class Menu extends React.Component {
       { to: "/", label: "Home", icon: FaHome },
       { to: "/category/", label: "Categories", icon: FaTag },
       { to: "/search/", label: "Search", icon: FaSearch },
-      ...pages,
-      { to: "/contact/", label: "Contact", icon: FaEnvelope }
     ];
 
     this.renderedItems = []; // will contain references to rendered DOM elements of menu
