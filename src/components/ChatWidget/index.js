f6cb32c2626924b368aef7e5775eb3add859436f
Rameez Khan <rameezkhan.sa@gmail.com>
2019-02-25 14:58:14 +0200

    Change chat widget colours

--- src/components/ChatWidget/index.js
+++ src/components/ChatWidget/index.js
@@ -1,17 +1,15 @@
+import "react-chat-widget/lib/styles.css";
+
 import React from "react";
 
 import avatar from "../../images/jpg/avatar.jpg";
-
-import "react-chat-widget/lib/styles.css";
 
 let chatWidget;
 
 class ChatWidget extends React.Component {
   constructor() {
     super();
-    this.state = {
-      showChat: false
-    };
+    this.state = { showChat: false };
   }
 
   render() {
@@ -23,6 +21,17 @@ class ChatWidget extends React.Component {
             subtitle="What can I help you with today?"
             profileAvatar={avatar}
           />
+          <style jsx>
+            {`
+              :global(.rcw-launcher) {
+                background-color: #709425;
+              }
+
+              :global(.rcw-conversation-container > .rcw-header) {
+                background-color: #709425;
+              }
+            `}
+          </style>
         </React.Fragment>
       )
     );
